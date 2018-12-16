fcc69a24feb716b7706d7ee2a6f7fc92a05c3a43
Bartłomiej Kudzia <bar.kud.11340@gmail.com>
2018-12-16 16:38:50 +0100

    Updated window class

--- src/main/java/pl/qqlka/gametest/App.java
+++ src/main/java/pl/qqlka/gametest/App.java
@@ -6,8 +6,7 @@ import pl.qqlka.gametest.display.Window;
 
 public class App {
     public static void main(String[] args) {
-        Window window = new Window();
-        window.init(800, 600, "Hello");
+        Window window = new Window(1280, 720, "Title", true, 4, 5);
         
         while(!window.shouldClose()) {
             window.clear(0.5f, 0.5f, 0.1f, 1.0f);
--- src/main/java/pl/qqlka/gametest/display/Window.java
+++ src/main/java/pl/qqlka/gametest/display/Window.java
@@ -2,78 +2,80 @@ package pl.qqlka.gametest.display;
 
 import static org.lwjgl.glfw.GLFW.*;
 
+import org.lwjgl.glfw.GLFWVidMode;
 import org.lwjgl.opengl.GL;
 import static org.lwjgl.opengl.GL11.*;
 
 public class Window {
     private long windowHandle;
-    boolean isInitialized;
 
-    public Window() {
-        isInitialized = false;
-    }
-
-    public void init(int width, int height, String title) throws RuntimeException {
-        if(GLFW_NOT_INITIALIZED != 0)
-            glfwInit();
-
+    public Window(int width, int height, String title) throws RuntimeException {
+        glfwInit();
         glfwDefaultWindowHints();
 
-        windowHandle = glfwCreateWindow(width, height, title, 0, 0);
-        if(windowHandle == 0) {
-            throw new RuntimeException("Cannot create glfw window");
-        }
-        glfwMakeContextCurrent(windowHandle);
-
-        isInitialized = true;
-        GL.createCapabilities();
+        createWindow(width, height, title, false);
     }
 
-    public void init(int width, int height, String title, int glMajorVersion, int glMinorVersion) throws RuntimeException {
-        if(GLFW_NOT_INITIALIZED != 0)
-            glfwInit();
+    public Window(int width, int height, String title, boolean fullscreen) throws RuntimeException {
+        glfwInit();
+        glfwDefaultWindowHints();
 
+        createWindow(width, height, title, fullscreen);
+    }
+
+    public Window(int width, int height, String title, int glMajorVersion, int glMinorVersion) throws RuntimeException {
+        glfwInit();
         glfwDefaultWindowHints();
 
         glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
         glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, glMajorVersion);
         glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, glMinorVersion);
 
-        windowHandle = glfwCreateWindow(width, height, title, 0, 0);
+        createWindow(width, height, title, false);
+    }
+
+    public Window(int width, int height, String title, boolean fullscreen, int glMajorVersion, int glMinorVersion) {
+        glfwInit();
+        glfwDefaultWindowHints();
+
+        glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+        glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, glMajorVersion);
+        glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, glMinorVersion);
+
+        createWindow(width, height, title, fullscreen);
+    }
+
+    private void createWindow(int width, int height, String title, boolean fullscreen) throws RuntimeException {
+        long monitor = 0;
+
+        if(fullscreen) {
+            monitor = glfwGetPrimaryMonitor();
+        }
+
+        windowHandle = glfwCreateWindow(width, height, title, monitor, 0);
         if(windowHandle == 0) {
-            throw new RuntimeException("Cannot create glfw window");
+            throw new RuntimeException("Cannot create window");
         }
         glfwMakeContextCurrent(windowHandle);
 
-        isInitialized = true;
         GL.createCapabilities();
     }
 
     public boolean shouldClose() {
-        if(isInitialized) {
-            return glfwWindowShouldClose(windowHandle);
-        } else {
-            return true;
-        }
+        return glfwWindowShouldClose(windowHandle);
     }
 
     public void update() {
-        if(isInitialized) {
-            glfwSwapBuffers(windowHandle);
-            glfwPollEvents();
-        }
+        glfwSwapBuffers(windowHandle);
+        glfwPollEvents();
     }
 
     public void destroy() {
-        if(isInitialized) {
-            glfwDestroyWindow(windowHandle);
-        }
+        glfwDestroyWindow(windowHandle);
     }
 
     public void clear(float red, float green, float blue, float alpha) {
-        if(isInitialized) {
-            glClearColor(red, green, blue, alpha);
-            glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-        }
+        glClearColor(red, green, blue, alpha);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
     }
 }
